83b829635c0fce4fce3452011cec31ab9c43db46
Pierre-Loup Tristant <pl.tristant@gmail.com>
2012-02-08 21:44:30 +0100

    Mise a jours des licences des fichiers

--- Taxinomes/main.m
+++ Taxinomes/main.m
@@ -2,9 +2,26 @@
 //  main.m
 //  Taxinomes
 //
-//  Created by mac adam on 13/10/11.
-//  Copyright 2011 Les petits débrouillards Bretagne. All rights reserved.
+//  Created by Pierre-Loup Tristant on 13/10/11.
+//  Copyright (c) 2011 Les petits débrouillards Bretagne. All rights reserved.
 //
+
+/*
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ Foobar is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with Foobar.  If not, see <http://www.gnu.org/licenses/>
+ 
+ */
 
 #import <UIKit/UIKit.h>
 
